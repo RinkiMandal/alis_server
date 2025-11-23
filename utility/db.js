78586bbe88e77bb5d817d98667720ae864dc7b19
RinkiMandal <rinkimandal944@gmail.com>
2025-11-23 21:18:00 +0530

    test

--- utility/db.js
+++ utility/db.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const mongoUrl = process.env.DB_URL;
+// const mongoUrl = process.env.DB_URL;
+const mongoUrl ='mongodb://localhost:27017/cakedata'
+
 
 export const connectToMongo = async () => {
   try {
